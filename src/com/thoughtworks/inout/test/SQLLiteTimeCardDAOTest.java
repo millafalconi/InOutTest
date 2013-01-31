b0e3d6e11b43453637c30250488aac93c8080705
Leandro Gualter <leandrogualter@gmail.com>
2013-01-31 17:39:53 -0300

    Tests for punch activity

--- src/com/thoughtworks/inout/test/SQLLiteTimeCardDAOTest.java
+++ src/com/thoughtworks/inout/test/SQLLiteTimeCardDAOTest.java
@@ -5,27 +5,17 @@ import static org.mockito.Mockito.verify;
 
 import java.util.Date;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.test.InstrumentationTestCase;
 
 import com.thoughtworks.inout.Punch;
 import com.thoughtworks.inout.PunchType;
 import com.thoughtworks.inout.TimeCard;
 import com.thoughtworks.inout.clock.TimeMachine;
+import com.thoughtworks.inout.db.SQLLiteTimeCardDAO;
 import com.thoughtworks.inout.db.TimeCardDAO;
 
-public class InOutTest extends InstrumentationTestCase {
-	
-	
-	public void testShouldRegisterInTimeCard(){
-
-		TimeCardDAO mockTimeCardDAO = mock(TimeCardDAO.class);
-		TimeCard timeCard = new TimeCard(mockTimeCardDAO);
-		Date now = TimeMachine.freeze();
-			
-		PunchType punchIn = PunchType.IN;
-		timeCard.punch(punchIn);
-	
-		verify(mockTimeCardDAO).insertPunch(new Punch(punchIn, now));
-	}
+public class SQLLiteTimeCardDAOTest extends InstrumentationTestCase{
 	
 }
